c1af67053d7a443cf932ca2684c27a04f30c005e
Manuele Ferreira <manueleferreira@gmail.com>
2016-11-28 16:31:55 -0200

    create base unit and integration test

---
+++ cloudapi/src/test/java/br/com/cloudapi/controller/RunInstanceControllerTest.java
@@ -0,0 +1,35 @@
+package br.com.cloudapi.controller;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+/**
+ * Created by manuele on 28/11/16.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class RunInstanceControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void getRunInstance() throws Exception
+    {
+        mvc.perform(MockMvcRequestBuilders.get("/runinstance")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("{\"id\":1,\"content\":\"World\"}")));
+    }
+}
---
+++ cloudapi/src/test/java/br/com/cloudapi/integration/RunInstanceIT.java
@@ -0,0 +1,45 @@
+package br.com.cloudapi.integration;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import java.net.URL;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by manuele on 28/11/16.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class RunInstanceIT {
+
+    @LocalServerPort
+    private int port;
+
+    private URL base;
+
+    @Autowired
+    private TestRestTemplate template;
+
+    @Before
+    public void setUp() throws Exception {
+        String infoBase = String.format("http://localhost:%s/runinstance", port);
+        this.base = new URL(infoBase);
+    }
+
+    @Test
+    public void getRunInstance() throws Exception {
+        ResponseEntity<String> response = template.getForEntity(base.toString(), String.class);
+        assertThat(response.getBody(), equalTo("{\"id\":1,\"content\":\"World\"}"));
+    }
+
+}
