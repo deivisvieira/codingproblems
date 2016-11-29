94bc05315a464423dd4fb8660ee6372c929df12a
Manuele Ferreira <manueleferreira@gmail.com>
2016-11-29 08:33:18 -0200

    update login

--- cloudapi/src/main/java/br/com/cloudapi/WebSecurityConfig.java
+++ cloudapi/src/main/java/br/com/cloudapi/WebSecurityConfig.java
@@ -1,23 +1,35 @@
 package br.com.cloudapi;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
 /**
- * Created by manuele on 28/11/16.
+ * Created by manuele on 29/11/16.
  */
-@EnableWebSecurity
-@Configuration
-public class WebSecurityConfig extends
-        WebSecurityConfigurerAdapter {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+        http.authorizeRequests()
+            .antMatchers("/", "/home").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin()
+            .loginPage("/login")
+            .permitAll()
+            .and()
+            .logout()
+            .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("user").password("password").roles("USER");
     }
 }
